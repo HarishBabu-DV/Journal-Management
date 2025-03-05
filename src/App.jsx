c42a4464cc8f2387cdf285ed09387989dc310aac
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-05 12:59:16 +0530

    created journal management app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    // App Container  
+    <main className='max-w-[1920px] mx-auto w-full'>
+      {/* Header  */}
+      <Header />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        {/* Logo and user profile Container  */}
+        <section className='w-full h-[10vh] bg-slate-200'>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </section>
+        {/* Navigation bar  */}
+        <nav>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
