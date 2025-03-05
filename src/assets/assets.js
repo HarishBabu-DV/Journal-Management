2025d416553eb08c68ac7660b8328df53f149351
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-05 23:34:55 +0530

    modified nav height

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,60 @@
 import logo from './images/smerlogo.png'
 import logowhite from './images/smerlogo(white).png'
-import { FaRegCircleUser } from "react-icons/fa6";
+import { FaRegCircleUser,FaChevronDown  } from "react-icons/fa6";
 import { CircleUser } from 'lucide-react';
-export {logo,logowhite,FaRegCircleUser,CircleUser}
+
+//Nav Items
+export const navItems={
+    firstSet:"Home",
+    secondSet:[
+        {
+            id:1,
+            navItem:"the journal"
+        },
+        {
+            id:2,
+            navItem:"author guidelines"
+        },
+        {
+            id:3,
+            navItem:"submission & review process"
+        },
+        {
+            id:4,
+            navItem:"current & past issues"
+        },
+        {
+            id:5,
+            navItem:"research ethics and policies"
+        },
+        {
+            id:6,
+            navItem:"indexing and abstracting"
+        },
+        {
+            id:7,
+            navItem:"reviewer guidelines"
+        }
+    ],
+    thirdSet:[
+        {
+            id:1,
+            navItem:"editorial board"
+        },
+        {
+            id:2,
+            navItem:"call for papers"
+        },
+        {
+            id:3,
+            navItem:"contact us"
+        },
+        {
+            id:4,
+            navItem:"faqs"
+        }
+    ]
+}
+
+
+export {logo,logowhite,FaRegCircleUser,FaChevronDown}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router'
-import { logo,FaRegCircleUser, CircleUser } from '../assets/assets'
+import { logo,FaRegCircleUser, FaChevronDown, navItems } from '../assets/assets'
 
 const Header = () => {
+  // About navitem toggle 
+  const [isAboutOpen,setIsAboutOpen]=useState(false)
   return (
-    <header className='w-full h-[20vh] '>
+    <header className='w-[80%] mx-auto'>
+
         {/* Logo and user profile Container  */}
-        <section className='flex items-center justify-between w-full h-1/2 px-[8%]'>
-            <Link className='' to={"/"}>
+        <section className='flex items-center justify-between w-full py-2' >
+            <Link className='flex' to={"/"} >
                 <div className='w-[50px] h-[50px] max-sm:w-[44px] max-sm:h-[44px]'>
                     <img src={logo} alt="" className='w-full h-full'/>
                 </div>
             </Link>
             <Link className='' to={"/"}>
-              <FaRegCircleUser className='text-3xl text-[#636363] max-sm:text-[1.6rem]'/>
+              <FaRegCircleUser className='text-[2rem] text-[#494949] max-sm:text-[1.6rem]'/>
             </Link>
         </section>
-        {/* Navigation bar  */}
-        <nav>
 
+        {/* Navigation bar  */}
+        <nav className='flex items-center justify-center gap-8 bg-[#2e2e2e] relative '>
+          {/* Home  */}
+          <Link to={'/'} className='text-white text-[1.1rem] single-navitem'> {navItems.firstSet} </Link>
+          {/* About  */}
+          <div className='relative hover:cursor-pointer about-container' onClick={()=>setIsAboutOpen(!isAboutOpen)}>
+            <div className='flex items-center gap-2 single-navitem '>
+              <span className='text-white '>About</span>
+              <span className='text-white'><FaChevronDown /></span>
+            </div>
+            {/* Hidden navitems container  */}
+            <div style={isAboutOpen?{display:"block"}:{display:"none"}}  className='absolute top-[150%] left-0 w-[300px] flex flex-col  hidden-navitems'>
+              {
+                navItems.secondSet.map((navItem)=>(
+                  <div className='px-4 py-2 hover:bg-[#cccccc94] flex hidden-navitems-navitem'>
+                    <Link to={'/'} className='text-[1.1rem] capitalize ' key={navItem.id}>{navItem.navItem}</Link>
+                  </div>  
+                ))
+              }
+            </div>
+          </div>
+          {/*Editorial Board,..,faqs*/}
+          {
+            navItems.thirdSet.map((navItem)=>(
+              <Link to={'/'} className='text-white text-[1.1rem] capitalize single-navitem' key={navItem.id}>{navItem.navItem}</Link>
+            ))
+          }
         </nav>
+
     </header>
   )
 }
