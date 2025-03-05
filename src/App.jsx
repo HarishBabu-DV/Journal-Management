971ab020ed7e12c83351dee9775d20768fe0778d
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-05 13:28:38 +0530

    created header section and installed react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import { Route, Routes } from 'react-router'
 
 const App = () => {
   return (
@@ -7,6 +8,9 @@ const App = () => {
     <main className='max-w-[1920px] mx-auto w-full'>
       {/* Header  */}
       <Header />
+      {/* <Routes>
+        <Route path='/' element={<Header />} />
+      </Routes> */}
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router'
+import logo from '../assets/assets'
 
 const Header = () => {
   return (
-    <header>
+    <header className='w-full h-[10vh] bg-slate-200'>
         {/* Logo and user profile Container  */}
-        <section className='w-full h-[10vh] bg-slate-200'>
-            <div>
-                <div>
-                    <img src="" alt="" />
+        <section className=''>
+            <Link className='' to={"/"}>
+                <div className='w-[60px] h-[60px]'>
+                    <img src={logo} alt="" className='w-full h-full'/>
                 </div>
-            </div>
-            <div>
-                
-            </div>
+            </Link>
+            <Link className='' to={"/"}>
+              
+            </Link>
         </section>
         {/* Navigation bar  */}
         <nav>
