64039146b3851288f127c7e4bf482abc2e4ac0ef
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-06 00:04:26 +0530

    modified navbar properties

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,35 +16,38 @@ const Header = () => {
                 </div>
             </Link>
             <Link className='' to={"/"}>
-              <FaRegCircleUser className='text-[2rem] text-[#494949] max-sm:text-[1.6rem]'/>
+              <FaRegCircleUser className='text-[1.8rem] text-[#494949] max-sm:text-[1.6rem]'/>
             </Link>
         </section>
 
         {/* Navigation bar  */}
-        <nav className='flex items-center justify-center gap-8 bg-[#2e2e2e] relative '>
+        <nav className='flex items-center gap-10 bg-[#2e2e2e] relative pl-10'>
+          
           {/* Home  */}
-          <Link to={'/'} className='text-white text-[1.1rem] single-navitem'> {navItems.firstSet} </Link>
+          <Link to={'/'} className='text-white py-[.3rem]  hover:text-[#1b9ce3] transition-colors'> {navItems.firstSet} </Link>
+          
           {/* About  */}
-          <div className='relative hover:cursor-pointer about-container' onClick={()=>setIsAboutOpen(!isAboutOpen)}>
-            <div className='flex items-center gap-2 single-navitem '>
+          <div className='relative hover:cursor-pointer about-navitem-container' onClick={()=>setIsAboutOpen(!isAboutOpen)}>
+            <div className='flex items-center gap-2 about-navitem'>
               <span className='text-white '>About</span>
-              <span className='text-white'><FaChevronDown /></span>
+              <span className='text-white '><FaChevronDown /></span>
             </div>
             {/* Hidden navitems container  */}
-            <div style={isAboutOpen?{display:"block"}:{display:"none"}}  className='absolute top-[150%] left-0 w-[300px] flex flex-col  hidden-navitems'>
+            <div style={isAboutOpen?{display:"block"}:{display:"none"}}  className='absolute top-[130%] left-0 w-[300px] flex flex-col hidden-navitems'>
               {
                 navItems.secondSet.map((navItem)=>(
-                  <div className='px-4 py-2 hover:bg-[#cccccc94] flex hidden-navitems-navitem'>
-                    <Link to={'/'} className='text-[1.1rem] capitalize ' key={navItem.id}>{navItem.navItem}</Link>
+                  <div className='px-4  hover:bg-[#cccccc94] flex hidden-navitems-navitem'>
+                    <Link to={'/'} className='capitalize py-[.3rem] px-[1.5rem] transition-colors ' key={navItem.id}>{navItem.navItem}</Link>
                   </div>  
                 ))
               }
             </div>
           </div>
+          
           {/*Editorial Board,..,faqs*/}
           {
             navItems.thirdSet.map((navItem)=>(
-              <Link to={'/'} className='text-white text-[1.1rem] capitalize single-navitem' key={navItem.id}>{navItem.navItem}</Link>
+              <Link to={'/'} className='text-white  capitalize py-[.3rem] hover:text-[#1b9ce3] transition-colors' key={navItem.id}>{navItem.navItem}</Link>
             ))
           }
         </nav>
