4522a81316c4a24d8f9a3711c40dcd75b7c02e15
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-05 14:10:21 +0530

    user profile size modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
         {/* Logo and user profile Container  */}
         <section className='flex items-center justify-between w-full h-1/2 px-[8%]'>
             <Link className='' to={"/"}>
-                <div className='w-[40px] h-[40px] '>
+                <div className='w-[50px] h-[50px] max-sm:w-[44px] max-sm:h-[44px]'>
                     <img src={logo} alt="" className='w-full h-full'/>
                 </div>
             </Link>
             <Link className='' to={"/"}>
-              <FaRegCircleUser className='text-3xl max-sm:text-[1.6rem]'/>
+              <FaRegCircleUser className='text-3xl text-[#636363] max-sm:text-[1.6rem]'/>
             </Link>
         </section>
         {/* Navigation bar  */}
