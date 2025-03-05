1172c3921f4396cc2f422ca1c60dc06f8894eb4b
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-05 13:49:40 +0530

    logo size updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <header className='w-full h-[20vh] '>
         {/* Logo and user profile Container  */}
-        <section className='flex items-center justify-between w-full h-1/2  px-[10%]'>
+        <section className='flex items-center justify-between w-full h-1/2 px-[8%]'>
             <Link className='' to={"/"}>
-                <div className='w-[40px] h-[40px]'>
+                <div className='w-[40px] h-[40px] max-sm:w-[35px] max-sm:h-[35px]'>
                     <img src={logo} alt="" className='w-full h-full'/>
                 </div>
             </Link>
             <Link className='' to={"/"}>
-              <FaRegCircleUser className='text-3xl' />
+              <FaRegCircleUser className='text-3xl max-sm:text-2xl' />
             </Link>
         </section>
         {/* Navigation bar  */}
