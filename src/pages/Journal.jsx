0c7dbb4330ed7c81e3210cbf9077fdc399d41097
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-07 20:23:12 +0530

    created contents for home page

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -5,7 +5,7 @@ const Journal = () => {
 
   const currentLocation=useLocation()
   return (
-    <section className='w-full pt-[130px] pl-[10%] pr-[27%] max-lg:pl-[5%] max-lg:pr-[22%] max-md:pr-[5%] max-md:pt-[80px]'>
+    <article className='w-full pt-[130px] pl-[10%] pr-[27%] max-lg:pl-[5%] max-lg:pr-[22%] max-md:pr-[5%] max-md:pt-[80px]'>
          {/* Path  */}
          <div>
             {
@@ -14,7 +14,18 @@ const Journal = () => {
                     null
             }
         </div>
-    </section>
+
+        {/* Aim  */}
+        <div>
+          <h1 className='text-[#134275] border-b-[3px] py-4 border-gray-400 text-3xl font-bold'>Aims</h1>
+          
+          <p className='py-4 font-normal text-[1rem] text-[#555555] '>
+          JMELL aims to promote high-quality research in modern English language and literature by fostering innovative ideas, theoretical advancements, and methodological diversity. The journal seeks to facilitate intellectual discussions that enhance understanding of contemporary linguistic and literary developments. By providing a global platform for scholarly exchange, JMELL encourages original research that contributes to the evolving discourse in English studies.
+          </p>
+        </div>
+
+      
+    </article>
   )
 }
 
