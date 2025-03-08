618bcae1d94bfc6d637145a255cb93909096e3ee
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-08 18:49:56 +0530

    layout for editorial board completed

--- src/pages/EditorialBoard.jsx
+++ src/pages/EditorialBoard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
-const EditorialBoard = () => {
+const EditorialBoard = ({classname}) => {
+   // To retrieve routes 
+   const currentLocation=useLocation()
   return (
-    <div>EditorialBoard</div>
+    <article className={`${classname} max-md:pt-[80px]`}>
+    {/* Path  */}
+    <div>
+          {
+            currentLocation?.pathname === '/editorial-board' ?
+                <p className='text-gray-400 font-medium'>About {">"} Editorial Board</p> :
+                null
+          }
+    </div>
+
+    {/* Heading  */}
+    <h1 className='heading-style'>Editorial Board</h1>
+    </article>
   )
 }
 
