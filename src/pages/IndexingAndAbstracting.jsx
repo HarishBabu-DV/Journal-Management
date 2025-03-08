bf12650859c84dc296a882edb9974afa971954e6
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-08 18:48:04 +0530

    layout for reviewerpage completed

--- src/pages/IndexingAndAbstracting.jsx
+++ src/pages/IndexingAndAbstracting.jsx
@@ -5,7 +5,19 @@ const IndexingAndAbstracting = ({classname}) => {
    // To retrieve routes 
    const currentLocation=useLocation()
   return (
-    <div>IndexingAndAbstracting</div>
+    <article className={`${classname} max-md:pt-[80px]`}>
+    {/* Path  */}
+    <div>
+          {
+            currentLocation?.pathname === '/about/indexing-and-abstracting' ?
+                <p className='text-gray-400 font-medium'>About {">"} Indexing and Abstracting</p> :
+                null
+          }
+    </div>
+
+    {/* Heading  */}
+    <h1 className='heading-style'>Indexing and Abstracting</h1>
+    </article>
   )
 }
 
--- src/pages/ReviewerGuidelines.jsx
+++ src/pages/ReviewerGuidelines.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
-const ReviewerGuidelines = () => {
+const ReviewerGuidelines = ({classname}) => {
+   // To retrieve routes 
+   const currentLocation=useLocation()
   return (
-    <div>ReviewerGuidelines</div>
+    <article className={`${classname} max-md:pt-[80px]`}>
+    {/* Path  */}
+    <div>
+          {
+            currentLocation?.pathname === '/about/reviewer-guidelines' ?
+                <p className='text-gray-400 font-medium'>About {">"} Reviewer Guidelines</p> :
+                null
+          }
+    </div>
+
+    {/* Heading  */}
+    <h1 className='heading-style'>Reviewer Guidelines</h1>
+    </article>
   )
 }
 
