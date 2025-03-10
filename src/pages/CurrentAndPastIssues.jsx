16e1055d5dc95ae59b59710f535acdb116f29e8c
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-10 17:17:08 +0530

    removed path for current and past issues

--- src/pages/CurrentAndPastIssues.jsx
+++ src/pages/CurrentAndPastIssues.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
-import { useLocation } from 'react-router'
-
-const CurrentAndPastIssues = ({classname}) => {
-   // To retrieve routes 
-   const currentLocation=useLocation()
+const CurrentAndPastIssues = () => {
   return (
-    <article className={`${classname}`}>
-    {/* Path  */}
-    <div>
-          {
-            currentLocation?.pathname === '/about/current-and-past-issues' ?
-                <p className='text-gray-400 font-medium'>About {">"} Current and Past Issues</p> :
-                null
-          }
-    </div>
-
-    {/* Heading  */}
-    <h1 className='heading-style'>Current and Past Issues</h1>
-  </article>
+    <article >
+      {/* Heading  */}
+      <h1 className='heading-style'>Current and Past Issues</h1>
+    </article>
   )
 }
 
