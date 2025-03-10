4a19505a54878ffc69de833bf3c4d4b0258f7a10
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-10 17:19:54 +0530

    removed path for review,indexing

--- src/pages/IndexingAndAbstracting.jsx
+++ src/pages/IndexingAndAbstracting.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import { useLocation } from 'react-router'
-
-const IndexingAndAbstracting = ({classname}) => {
-   // To retrieve routes 
-   const currentLocation=useLocation()
+const IndexingAndAbstracting = () => {
   return (
-    <article className={`${classname} `}>
-    {/* Path  */}
-    <div>
-          {
-            currentLocation?.pathname === '/about/indexing-and-abstracting' ?
-                <p className='text-gray-400 font-medium'>About {">"} Indexing and Abstracting</p> :
-                null
-          }
-    </div>
-
-    {/* Heading  */}
-    <h1 className='heading-style'>Indexing and Abstracting</h1>
+    <article>
+      {/* Heading  */}
+      <h1 className='heading-style'>Indexing and Abstracting</h1>
     </article>
   )
 }
--- src/pages/ReviewerGuidelines.jsx
+++ src/pages/ReviewerGuidelines.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import { useLocation } from 'react-router'
-
-const ReviewerGuidelines = ({classname}) => {
-   // To retrieve routes 
-   const currentLocation=useLocation()
+const ReviewerGuidelines = () => {
   return (
-    <article className={`${classname} `}>
-    {/* Path  */}
-    <div>
-          {
-            currentLocation?.pathname === '/about/reviewer-guidelines' ?
-                <p className='text-gray-400 font-medium'>About {">"} Reviewer Guidelines</p> :
-                null
-          }
-    </div>
-
-    {/* Heading  */}
-    <h1 className='heading-style'>Reviewer Guidelines</h1>
+    <article>
+      {/* Heading  */}
+      <h1 className='heading-style'>Reviewer Guidelines</h1>
     </article>
   )
 }
