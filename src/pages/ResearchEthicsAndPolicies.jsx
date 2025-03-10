b53af68ec044ffd9358c01268eb47ba6aa3538ba
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-10 17:18:02 +0530

    removed path for research

--- src/pages/ResearchEthicsAndPolicies.jsx
+++ src/pages/ResearchEthicsAndPolicies.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { useLocation } from 'react-router'
 
-const ResearchEthicsAndPolicies = ({classname}) => {
-   // To retrieve routes 
-   const currentLocation=useLocation()
+const ResearchEthicsAndPolicies = () => {
   return (
-    <article className={`${classname} `}>
-    {/* Path  */}
-    <div>
-          {
-            currentLocation?.pathname === '/about/research-ethics-and-policies' ?
-                <p className='text-gray-400 font-medium'>About {">"} Research Ethics and Policies</p> :
-                null
-          }
-    </div>
-
-    {/* Heading  */}
-    <h1 className='heading-style'>Research Ethics and Policies</h1>
+    <article>
+      {/* Heading  */}
+      <h1 className='heading-style'>Research Ethics and Policies</h1>
     </article>
   )
 }
