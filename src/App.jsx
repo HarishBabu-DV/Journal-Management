399271f53615bf77cd88638519a6c17bab068b9b
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-10 17:29:57 +0530

    modified and imported sidebar as component

--- src/App.jsx
+++ src/App.jsx
@@ -14,50 +14,48 @@ import CallForPapers from './pages/CallForPapers'
 import ContactUs from './pages/ContactUs'
 import Faqs from './pages/Faqs'
 import BreadCrumb from './components/BreadCrumb'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
     <>
-    {/* Header  */}
-    <Header />
-    
-    {/* App Container   */}
-    <main className='max-w-[1920px] mx-auto px-[10%] max-lg:px-[5%] pt-[130px] max-lg:pt-[80px] w-full flex gap-10'>
-        
-        <section className='w-[80%] max-lg:w-full'>
-          {/* Breadcrumb  */}
-          <BreadCrumb />
-          <Routes>
-            {/* Home  */}
-            <Route path='/' element={<Home />} />
-            {/* About  */}
-            <Route path='/about/journal' element={<Journal/>} /> 
-            <Route path='/about/author-guidelines' element={<AuthorGuidelines />} />
-            <Route path='/about/submission-and-review-process' element={<SubmissionAndReviewProcess />} />
-            <Route path='/about/current-and-past-issues' element={<CurrentAndPastIssues />} />
-            <Route path='/about/research-ethics-and-policies' element={<ResearchEthicsAndPolicies />} />
-            <Route path='/about/indexing-and-abstracting' element={<IndexingAndAbstracting/>} />
-            <Route path='/about/reviewer-guidelines' element={<ReviewerGuidelines />} />
+      {/* Header  */}
+      <Header />
+      
+      {/* Main   */}
+      <main className='max-w-[1920px] mx-auto px-[10%] max-lg:px-[5%] pt-[130px] max-lg:pt-[80px] w-full flex gap-10'>
+          
+          <section className='w-[80%] max-lg:w-full'>
+            {/* Breadcrumb  */}
+            <BreadCrumb />
+            <Routes>
+              {/* Home  */}
+              <Route path='/' element={<Home />} />
 
-            {/* Editorial Board  */}
-            <Route path='/editorial-board' element={<EditorialBoard />} />
-            {/* Call For Papers  */}
-            <Route path='/call-for-papers' element={<CallForPapers/>} />
-            {/* Contact Us  */}
-            <Route path='/contact-us' element={<ContactUs/>} />
-            {/* FAQ's  */}
-            <Route path='/faqs' element={<Faqs/>} />
+              {/* About  */}
+              <Route path='/about/journal' element={<Journal/>} /> 
+              <Route path='/about/author-guidelines' element={<AuthorGuidelines />} />
+              <Route path='/about/submission-and-review-process' element={<SubmissionAndReviewProcess />} />
+              <Route path='/about/current-and-past-issues' element={<CurrentAndPastIssues />} />
+              <Route path='/about/research-ethics-and-policies' element={<ResearchEthicsAndPolicies />} />
+              <Route path='/about/indexing-and-abstracting' element={<IndexingAndAbstracting/>} />
+              <Route path='/about/reviewer-guidelines' element={<ReviewerGuidelines />} />
 
-          </Routes>
-        </section>
-        
-        {/* Sidebar  */}
-        <aside className='w-[20%] border bg-gray-300 h-full mt-[130px] p-3 max-lg:hidden'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aperiam minima maiores, error quia eveniet magnam saepe vitae veritatis, quos dicta unde nisi repudiandae reiciendis eaque nemo reprehenderit! Odio, nobis?</p>
-        </aside>
+              {/* Editorial Board  */}
+              <Route path='/editorial-board' element={<EditorialBoard />} />
+              {/* Call For Papers  */}
+              <Route path='/call-for-papers' element={<CallForPapers/>} />
+              {/* Contact Us  */}
+              <Route path='/contact-us' element={<ContactUs/>} />
+              {/* FAQ's  */}
+              <Route path='/faqs' element={<Faqs/>} />
+            </Routes>
+          </section>
+          
+          {/* Sidebar  */}
+          <SideBar />
 
-    </main>
-
+      </main>
     </>
   )
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <aside className='w-[20%] border bg-gray-300 h-full mt-[130px] p-3 max-lg:hidden'>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aperiam minima maiores, error quia eveniet magnam saepe vitae veritatis, quos dicta unde nisi repudiandae reiciendis eaque nemo reprehenderit! Odio, nobis?
+        </p>
+    </aside>
+  )
+}
+
+export default SideBar
